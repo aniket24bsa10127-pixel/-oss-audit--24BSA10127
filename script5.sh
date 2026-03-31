5662ecc88689c7da722be14641b7d871a7b255dd
aniket24bsa10127-pixel <aniket.24bsa10127@vitbhopal.ac.in>
2026-03-31 20:22:03 +0530

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -2,9 +2,9 @@
 # Script 5: Open Source Manifesto Generator
 echo "Answer three questions to generate your manifesto."
 echo ""
-read -p "1. Name one open-source tool you use every day: " TOOL
-read -p "2. In one word, what does 'freedom' mean to you? " FREEDOM
-read -p "3. Name one thing you would build and share freely: " BUILD
+read -p "1. Name one open-source tool you use every day: " GitHub
+read -p "2. In one word, what does 'freedom' mean to you? " Universal
+read -p "3. Name one thing you would build and share freely: " Website
 DATE=$(date '+%d %B %Y')
 OUTPUT="manifesto_$(whoami).txt"
 echo "--- Open Source Manifesto ---" > "$OUTPUT"
